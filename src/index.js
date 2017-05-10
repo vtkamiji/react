e2a5787f61cc496b2b6597b73a6b155b15becda2
Valter Takahiro Kamiji - SUPDE/DECTA/DE904 <04355575993@serpro-1565731.(none)>
2017-05-10 16:54:10 -0300

    valter kamiji

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyB0BhseplUs2ApMYdbALyAnaxWeTx1mrkE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo: videos[0]
+            }); // == this.setState({videos: videos});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video = {this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                    videos = {this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
